f889cdb1310b5b4d4ae87793ad74218759300ad4
Einav Rivni <erivni@synamedia.com>
2021-12-26 16:37:10 +0200

    added drop rtp packet percentage option

--- pkg/nack/responder_interceptor.go
+++ pkg/nack/responder_interceptor.go
@@ -1,14 +1,14 @@
 package nack
 
 import (
-	"os"
-	"sync"
-
 	"github.com/pion/interceptor"
 	"github.com/pion/logging"
 	"github.com/pion/rtcp"
 	"github.com/pion/rtp"
 	log "github.com/sirupsen/logrus"
+	"os"
+	"strconv"
+	"sync"
 )
 
 // ResponderInterceptorFactory is a interceptor.Factory for a ResponderInterceptor
@@ -101,6 +101,20 @@ func (n *ResponderInterceptor) BindLocalStream(info *interceptor.StreamInfo, wri
 
 	return interceptor.RTPWriterFunc(func(header *rtp.Header, payload []byte, attributes interceptor.Attributes) (int, error) {
 		sendBuffer.add(&rtp.Packet{Header: *header, Payload: payload})
+
+		percentage, _ := strconv.ParseFloat(os.Getenv("HYPERSCALE_DROPPED_PACKET_PERCENTAGE"), 64)
+		if percentage > 0 && 100/percentage > 0 &&
+			header.SequenceNumber%(uint16(100/percentage)) == 0 {
+			log.WithFields(
+				log.Fields{
+					"subcomponent":            "interceptor",
+					"sequenceNumber":          header.SequenceNumber,
+					"droppedPacketPercentage": percentage,
+					"type":                    "INTENSIVE",
+				}).Warnf("dropping rtp packet with SN %d intentially..", header.SequenceNumber)
+			return 0, nil
+		}
+
 		return writer.Write(header, payload, attributes)
 	})
 }
@@ -127,7 +141,7 @@ func (n *ResponderInterceptor) resendPackets(nack *rtcp.TransportLayerNack) {
 					n.log.Warnf("failed resending nacked packet: %+v", err)
 				}
 			} else {
-				if os.Getenv("HYPERSCALE_WARN_MISSING_NACKED_PACKETS") == "true" {
+				if os.Getenv("HYPERSCALE_WARN_ON_MISSING_NACKED_PACKETS") == "true" {
 					log.WithFields(
 						log.Fields{
 							"subcomponent":   "interceptor",
